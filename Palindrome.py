302d26df88293f038a3613eb85b6b2ea9cc56f14
Sheth Uday <113230518+Sheth007@users.noreply.github.com>
2025-05-28 21:22:41 +0530

    Removed files

--- Palindrome.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        if '-' in str(x):
-            return False
-        if str(x) == str(x)[::-1]:
-            return True
-        else:
-            return False
